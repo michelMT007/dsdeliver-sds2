b8a1cae7cf05af21e07ea3cd53752ccd24e83388
unknown <michelifmtads@gmail.com>
2021-01-04 14:33:35 -0400

    Initial setup

---
+++ backend/src/main/resources/snippet/Snippet.java
@@ -0,0 +1,11 @@
+package snippet;
+
+public class Snippet {
+	spring.datasource.url=jdbc:h2:mem:testdb
+	spring.datasource.username=sa
+	spring.datasource.password=
+	
+	spring.h2.console.enabled=true
+	spring.h2.console.path=/h2-console
+}
+
