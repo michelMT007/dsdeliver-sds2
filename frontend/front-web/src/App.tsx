22f8143fe466013b0f7a6daba006edbddb108eb3
unknown <michelifmtads@gmail.com>
2021-01-06 20:37:09 -0400

    Create front-web innitials

---
+++ frontend/front-web/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Hello from './Hello';
+import Counter from './Counter';
+
+function App() {
+  return (
+    <div className="App">
+      <Counter/>
+    </div>  
+  );
+}
+
+export default App;
