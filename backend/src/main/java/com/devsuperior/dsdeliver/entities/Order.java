7a06a3418451ef4cb24be382c9aeccef6634b938
unknown <michelifmtads@gmail.com>
2021-01-11 11:47:53 -0400

    Fix total

--- backend/src/main/java/com/devsuperior/dsdeliver/entities/Order.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/entities/Order.java
@@ -100,6 +100,16 @@ public class Order implements Serializable{
 	public Set<Product> getProducts() {
 		return products;
 	}
+	
+	
+	public Double getTotal() {
+		double sum =0.0;
+		for(Product p: products) {
+			sum = sum + p.getPrice();
+		}
+		return sum;
+	}
+	
 
 	@Override
 	public int hashCode() {
