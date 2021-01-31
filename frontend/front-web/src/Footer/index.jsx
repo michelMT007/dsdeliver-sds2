d04f6c70fceff8fed6e246933dce03644401a1eb
michelMT007 <michelifmtads@gmail.com>
2021-01-31 00:40:29 -0400

    atualização da pagina

--- frontend/front-web/src/Footer/index.jsx
+++ frontend/front-web/src/Footer/index.jsx
@@ -7,17 +7,17 @@ function Footer(){
 
     return(
         <footer className="main-footer">
-            App desenvolvido duarante a 2 ed. do evento Semana DevSuperior
+            App desenvolvido por Michel DevSys
             <div>
-                <a href="https://www.youtube.com/c/DevSuperior" target="_new">
+                <a href="" target="_new">
                     <YouTubeIcon></YouTubeIcon>
                 </a>
 
-                <a href="https://www.linkedin.com/school/devsuperior//" target="_new">
+                <a href="" target="_new">
                     <LinknIcon></LinknIcon>
                 </a>
 
-                <a href="https://www.instagram.com/devsuperior.ig/" target="_new">
+                <a href="" target="_new">
                     <InstagramIcon></InstagramIcon>
                 </a>
             </div>
